0d1d55854e2c3bf75b451b9b0a48d7ae459e8e7c
Quentin <quentin.beaussart@gmail.com>
2018-11-08 18:47:33 +0100

    Vue accueil + game & declarations/listeners

--- OC_Quizz/app/src/main/java/com/laethel/android/oc_quizz/MainActivity.java
+++ OC_Quizz/app/src/main/java/com/laethel/android/oc_quizz/MainActivity.java
@@ -2,12 +2,48 @@ package com.laethel.android.oc_quizz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGreetingText=(TextView) findViewById(R.id.greeting_text_view);
+        mNameInput=(EditText) findViewById(R.id.greeting_edit_text);
+        mPlayButton=(Button) findViewById(R.id.greeting_button);
+        mPlayButton.setEnabled(false);
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPlayButton.setEnabled(s.toString().length() > 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
